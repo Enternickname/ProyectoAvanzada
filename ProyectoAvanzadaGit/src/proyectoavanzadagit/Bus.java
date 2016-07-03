57f332b208eb3dccbfd3b8b36130d51f4f8bd191
Sebastian <Sebastian@Basantes>
2016-07-03 15:21:01 -0500

    Llenado de las clases a usar

--- ProyectoAvanzadaGit/src/proyectoavanzadagit/Bus.java
+++ ProyectoAvanzadaGit/src/proyectoavanzadagit/Bus.java
@@ -1,7 +1,50 @@
 
 package proyectoavanzadagit;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Bus {
+    private String ruta;
+    private Date fecha;
+    private String horario;
+    private final Integer num_total_asientos;
+    private ArrayList <Boolean> num_asientos;
+
+    public Bus(String ruta, Date fecha, String horario) {
+        this.ruta = ruta;
+        this.fecha = fecha;
+        this.horario = horario;
+        this.num_total_asientos =55;
+        for(int i=0;i<this.num_total_asientos;i++)
+            this.num_asientos.add(true);
+    }
+    
+    
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
     
     
 }
